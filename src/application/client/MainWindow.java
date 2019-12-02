e142aeedebc83538beb04628a8c0cb41e589d71a
Marteeh <33930283+Marteeh@users.noreply.github.com>
2019-12-02 17:30:56 +0100

    start of main window implementation

--- src/application/client/MainWindow.java
+++ src/application/client/MainWindow.java
@@ -3,12 +3,48 @@ package application.client;
 import java.awt.event.*;
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.event.ListSelectionListener;
 
-public class MainWindow extends JFrame implements {
+public class MainWindow extends JFrame implements ListSelectionListener{
 
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+    
     private final EventQueue eventQueue;
+    private final String currentUser;
 
-    MainWindow(EventQueue eventQueue){
+    static JPanel panel;
+    static JList liste;
+    static DefaultListModel model;
+    static JLabel description;
+
+    MainWindow(String currentUser){
+
         this.eventQueue = eventQueue;
+        this.currentUser = currentUser;
+
+        panel = new JPanel(new GridLayout(1,1)); 
+        model = new DefaultListModel();
+        liste = new JList(model); 
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        add(panel, BorderLayout.CENTER);
+        setTitle("ChatSystem v9000 [" + currentUser + "] ");
+        setSize(300, 600);
+        setVisible(true);
+
+    }
+
+    public static void AddConnectedUser(String user){
+        //addElement, removeElement, set
+        model.addElement(user);
+    }
+
+    
+//pour tester
+    public static void main(String[] args){
+        new MainWindow("poupidou");
     }
 }
