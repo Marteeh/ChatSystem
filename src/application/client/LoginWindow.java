2235f7aaee92bfb857feef8497562967d56e803e
M. Toutant Martin <toutant@insa-20471>
2019-12-02 14:09:50 +0100

    login window implementation

--- src/application/client/LoginWindow.java
+++ src/application/client/LoginWindow.java
@@ -1,5 +1,37 @@
 package application.client;
 
-public class LoginWindow {
+import java.awt.event.*;
+import java.awt.*;
+import javax.swing.*;
 
+public class LoginWindow extends JFrame implements ActionListener {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	JPanel panel;
+	JLabel usrLabel, pswrdLabel, message;
+	JTextField usrnmTxt;
+	JPasswordField pswrdTxt;
+	JButton login, cancel;
+
+	LoginWindow() {
+
+		// username input
+		usrLabel = new JLabel();
+		usrLabel.setText("User Name :");
+		usrnmTxt = new JTextField();
+
+		// username input
+		pswrdLabel = new JLabel();
+		pswrdLabel.setText("User Name :");
+		pswrdTxt = new JPasswordField();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
 }
