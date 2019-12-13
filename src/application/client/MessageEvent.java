949c2cb7f9a80f19da77bfa1730156c31ce35642
M. Toutant Martin <toutant@insa-10226>
2019-12-13 14:36:29 +0100

    messageEvent correction

--- src/application/client/MessageEvent.java
+++ src/application/client/MessageEvent.java
@@ -5,12 +5,12 @@ public class MessageEvent extends GUIEvent {
 	public final User userFrom;
 	public final User userTo;
 	public final Date sendDate;
-    public final String content;
+	public final String content;
     
-    MessageEvent(User userFrom, User userTo, Date sendDate, String content) {
+	MessageEvent(User userFrom, User userTo, Date sendDate, String content) {
 		this.userFrom = username;
 		this.userTo = password;
-        this.sendDate = isExternal;
-        this.content = content;
+		this.sendDate = isExternal;
+		this.content = content;
 	}
 }
